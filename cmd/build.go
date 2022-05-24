84e03187c9702ced549304de896277f603bb8394
Thien Nguyen <tatthien.contact@gmail.com>
2022-05-24 17:07:52 +0700

    feat: polish the build output

--- cmd/build.go
+++ cmd/build.go
@@ -2,24 +2,33 @@ package cmd
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/tatthien/giraffe/engine"
 )
 
 func Build() {
+	start := time.Now()
+	fmt.Println("Start building site...")
+	fmt.Println("")
+
 	engine := engine.New()
-
 	engine.ScanContent()
-
 	engine.GenerateIndexPage()
 	engine.GenerateTagIndexPage()
 	engine.GenerateSingluarPages()
 	engine.GenerateTagPages()
 	engine.GenerateRSS()
 	engine.GeneratePostTypeArchive()
-
 	engine.CopyStaticFiles()
 
-	fmt.Printf("Generated: %d posts\n", len(engine.Posts))
-	fmt.Printf("Generated: %d tags\n", len(engine.Tags))
+	fmt.Println("  Content        | Total")
+	fmt.Println("-----------------+--------")
+	fmt.Printf("  Pages          | %d\n", len(engine.Posts))
+	fmt.Printf("  Tags           | %d\n", len(engine.Tags))
+	fmt.Printf("  Post types     | %d\n", len(engine.PostTypes))
+
+	duration := time.Since(start)
+	fmt.Println("")
+	fmt.Printf("Build time %s\n", duration)
 }
