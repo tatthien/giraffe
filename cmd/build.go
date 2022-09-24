36986c01d3a5ce21dfa94207ac6b4b02d92a78d7
Thien Nguyen <tatthien.contact@gmail.com>
2022-09-24 22:19:35 +0700

    chore: print build log using text/tabwriter

--- cmd/build.go
+++ cmd/build.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"text/tabwriter"
 	"time"
 
 	"github.com/tatthien/giraffe/engine"
@@ -9,8 +11,6 @@ import (
 
 func Build() {
 	start := time.Now()
-	fmt.Println("Start building site...")
-	fmt.Println("")
 
 	engine := engine.New()
 	engine.ScanContent()
@@ -21,14 +21,19 @@ func Build() {
 	engine.GenerateRSS()
 	engine.GeneratePostTypeArchive()
 	engine.CopyStaticFiles()
-
-	fmt.Println("  Content        | Total")
-	fmt.Println("-----------------+--------")
-	fmt.Printf("  Pages          | %d\n", len(engine.Posts))
-	fmt.Printf("  Tags           | %d\n", len(engine.Tags))
-	fmt.Printf("  Post types     | %d\n", len(engine.PostTypes))
-
 	duration := time.Since(start)
-	fmt.Println("")
-	fmt.Printf("Build time %s\n", duration)
+
+	rows := map[string]int{
+		"Pages":      len(engine.Posts),
+		"Tags":       len(engine.Tags),
+		"Post types": len(engine.PostTypes),
+	}
+
+	w := tabwriter.NewWriter(os.Stdout, 8, 8, 3, '\t', 0)
+	fmt.Fprintln(w, "CONTENT\tTOTAL")
+	for k, v := range rows {
+		fmt.Fprintf(w, "%s\t%d\n", k, v)
+	}
+	fmt.Fprintf(w, "%s\t%s\n", "Build time", duration)
+	w.Flush()
 }
